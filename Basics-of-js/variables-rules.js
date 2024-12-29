c84a35cc0113be9ed29c6c1b9de09c8d2cea3ef1
Hamza <khanzadahamza76@gmail.com>
2024-12-29 08:23:04 +0500

    rules of variables

---
+++ Basics-of-js/variables-rules.js
@@ -0,0 +1,20 @@
+// What is variables?
+// A variable is a container that's stores a value This is very similar to the container used to store rice , water , and owats (Treat's this is an analogy)
+
+// The Value of a js variable can be changed during the execution by a program
+
+
+// Rules for choosing Variables name;
+
+// letter, digits, underScore, & $sign Allowed
+// Must begin with a $,_ or a letter
+// Js reserved words can not be used as a variable name
+// Harry and haRRy are different Variable (case-sensitive)
+
+// How to create a variable
+
+ var a = 67;
+ console.log(a)
+ a = 'Harry'
+ console.log(a)
+
