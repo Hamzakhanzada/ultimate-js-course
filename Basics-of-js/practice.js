1b188008320df49a3136bae78b4f3a4959729554
Hamza <khanzadahamza76@gmail.com>
2024-12-29 09:37:23 +0500

    add js practice set

---
+++ Basics-of-js/practice.js
@@ -0,0 +1,21 @@
+// Create a variable of type string and try to add a number to it
+
+let a = 'Hello-World';
+let b = 5
+console.log(a+b)
+
+// use typeof opertaor to find the data type of the string in the last question;
+
+console.log(typeof (a+b));
+
+// create a const object in js can you change it to hold a number later ?
+
+const myDetails = {
+        name: 'Hamza',
+        age: 19,
+
+}
+// myDetails = 9
+myDetails['friend'] = "Ali";
+myDetails['name'] = "hamad";
+console.log(myDetails)
