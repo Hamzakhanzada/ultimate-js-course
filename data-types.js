715d4c969fe4fe7c53aa317c5d2ac4139ea60565
Hamza <khanzadahamza76@gmail.com>
2024-12-29 08:51:14 +0500

    add data types in js

---
+++ data-types.js
@@ -0,0 +1,24 @@
+// primitive data types and objects
+// primitive data type or a set of data types in javascript
+// and object is a non primitve data type in js
+
+
+// These are seven primitives data types in js
+
+// 1 : Number
+// 2 : Null
+// 3 : String
+// 4 : Symbol
+// 5 : Boolean
+// 6 : undefined
+// 7 : Bigint
+
+let a = 89;
+let b = null;
+let c = 'Hamza';
+let d = Symbol('Hey Iam a unique symbol');
+let e = true;
+let f = undefined;
+let g = BigInt(90);
+
+console.log(a,b,c,d,e,f,g);
